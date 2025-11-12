746278f953f7c70039fec89c98b8c03ddd55ab75
theoApporte <theo@saui.com.br>
2025-11-12 16:55:36 -0300

    basic functions

---
+++ main.py
@@ -0,0 +1,73 @@
+# main.py
+
+import os
+import uvicorn
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, EmailStr
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# --- Configuração do Banco de Dados ---
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    raise Exception("Erro: MONGO_URI não encontrada no ficheiro .env")
+
+client = AsyncIOMotorClient(MONGO_URI)
+
+db = client.onboardingApp
+
+collection_users = db.get_collection("users")
+
+
+# --- Modelos de Dados (Pydantic) ---
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserInDB(BaseModel):
+    email: EmailStr
+
+app = FastAPI()
+
+
+# --- Endpoints da API ---
+
+# Endpoint 1: Raiz (só para testar)
+@app.get("/")
+async def read_root():
+    return {"Mensagem": "API de Onboarding no ar!"}
+
+
+# Endpoint 2: Registar Utilizador
+@app.post("/register")
+async def register_user(user: UserRegister):
+    
+    existing_user = await collection_users.find_one({"email": user.email})
+    
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Este email já está registado.",
+        )
+
+    user_data = user.model_dump()
+    
+
+    # Insere o novo utilizador no banco de dados
+    new_user = await collection_users.insert_one(user_data)
+
+    # Retorna uma mensagem de sucesso
+    return {
+        "mensagem": "Utilizador registado com sucesso!",
+        "user_id": str(new_user.inserted_id)
+    }
+
+# --- rodar localmente ---
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
